e08d0beb95803d485d7461929b673c39f7db703a
dgvu <duongvu090903@gmail.com>
2018-12-01 17:54:27 +0700

    (.)

---
+++ bài tập về nhà/part 1.2.py
@@ -0,0 +1,13 @@
+from turtle import*
+
+shape("turtle")
+color("blue")
+
+for i in range(3):
+    for i in range(3):
+        forward(150)
+        left(90)
+    forward(150)
+    left(30)
+
+mainloop()
